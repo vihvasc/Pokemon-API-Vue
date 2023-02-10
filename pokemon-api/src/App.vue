8f8f3d04563332671df2123507d3dbef9c0f610d
Virginia <vihvasc@gmail.com>
2023-02-10 16:30:23 -0300

    Remove arquivos do Hello, world! e adiciona pokemon-logo.png e inicia estilização

--- pokemon-api/src/App.vue
+++ pokemon-api/src/App.vue
@@ -1,26 +1,19 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div>
+    <img alt="Pokemon Logo" id="pokemon-logo" src="./assets/pokemon-logo.png">
+  </div>
 </template>
 
-<script>
-import HelloWorld from './components/HelloWorld.vue'
-
-export default {
-  name: 'App',
-  components: {
-    HelloWorld
-  }
-}
-</script>
-
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+#pokemon-logo {
+  width: 300px;
+  height: 225px;
+  object-fit: fill;
+}
+
+div {
+  display: flex;
+  justify-content: center;
+  align-items: center;
 }
 </style>
