d2878679567cd3ec64eca269568d350a9361bc47
Virginia <vihvasc@gmail.com>
2023-02-11 11:12:37 -0300

    Filtro da barra de search consumindo corretamente a API

--- pokemon-api/vue.config.js
+++ pokemon-api/vue.config.js
@@ -2,5 +2,11 @@ const { defineConfig } = require('@vue/cli-service')
 module.exports = defineConfig({
   transpileDependencies: [
     'vuetify'
-  ]
+  ],
+
+  pluginOptions: {
+    vuetify: {
+			// https://github.com/vuetifyjs/vuetify-loader/tree/next/packages/vuetify-loader
+		}
+  }
 })
